87d9571f3ebbc49bdc0f13a850fd6bb651a6a69b
Greg Nestor <gregory.nestor@peelsb.com>
2016-11-18 16:58:06 -0500

    Added SimpleArm Subsystem and Command definitions

---
+++ src/org/usfirst/frc/team5519/subsystems/SimpleArm.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team5519.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class SimpleArm extends Subsystem {
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
